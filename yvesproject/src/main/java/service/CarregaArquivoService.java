5cc88089d07882c6afb9659200c7b1ac44489931
Yves <74190914+yvesls@users.noreply.github.com>
2023-01-18 11:11:12 -0300

    .

--- yvesproject/src/main/java/service/CarregaArquivoService.java
+++ yvesproject/src/main/java/service/CarregaArquivoService.java
@@ -8,10 +8,6 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-
 import DAO.DAOSQLiteFactory;
 import DAO.DAOSingleton;
 import bussiness.ConversorData;
